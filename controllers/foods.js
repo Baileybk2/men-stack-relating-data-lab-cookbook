a006d5d7ad1332d44348ee3a435cb7645f5b3123
BaileyBeck <baileybk2@gmail.com>
2024-07-05 16:12:18 -0400

    added create functionality and fixed mongoDB connection issue

--- controllers/foods.js
+++ controllers/foods.js
@@ -4,11 +4,28 @@ const router = express.Router();
 const User = require("../models/user.js");
 
 router.get("/", (req, res) => {
-  res.render("foods/index.ejs");
+  try {
+    res.render("foods/index.ejs");
+  } catch (error) {
+    console.log(error);
+    res.redirect("/");
+  }
 });
 
 router.get("/new", async (req, res) => {
   res.render("foods/new.ejs");
+});
+
+router.post("/", async (req, res) => {
+  try {
+    const currentUser = await User.findById(req.session.user._id);
+    currentUser.pantry.push(req.body);
+    await currentUser.save();
+    res.redirect(`/users/${currentUser._id}/foods`);
+  } catch (error) {
+    console.log(error);
+    res.redirect("/");
+  }
 });
 
 module.exports = router;
