da2dda65631bc44e3cfe90af001cb83403529346
BaileyBeck <baileybk2@gmail.com>
2024-07-06 12:36:52 -0400

    built update functionality

--- controllers/foods.js
+++ controllers/foods.js
@@ -69,4 +69,17 @@ router.delete("/:foodId", async (req, res) => {
   }
 });
 
+router.put("/:foodId", async (req, res) => {
+  try {
+    const currentUser = await User.findById(req.session.user._id);
+    const food = currentUser.pantry.id(req.params.foodId);
+    food.set(req.body);
+    await currentUser.save();
+    res.redirect(`/users/${currentUser._id}/foods/${req.params.foodId}`);
+  } catch (error) {
+    console.log(error);
+    res.redirect("/");
+  }
+});
+
 module.exports = router;
